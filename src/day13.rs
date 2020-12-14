cf0f62c6569d8736b5394dc9304f2f230bd1e7a7
Jonas Obrist <jonas.obrist@hennge.com>
2020-12-14 12:10:08 +0900

    stole day13p2

--- src/day13.rs
+++ src/day13.rs
@@ -1,4 +1,5 @@
 use crate::maybe_from::MaybeFrom;
+use crate::utils::Counter;
 use itertools::Itertools;
 use num::Integer;
 
@@ -47,8 +48,65 @@ impl MaybeFrom<&str> for Airport {
     }
 }
 
+#[inline(always)]
+fn check_timestamp(timestamp: usize) -> bool {
+    timestamp % 13 == 0
+        && (timestamp + 3) % 41 == 0
+        && (timestamp + 13) % 569 == 0
+        && (timestamp + 15) % 29 == 0
+        && (timestamp + 32) & 19 == 0
+        && (timestamp + 36) % 23 == 0
+        && (timestamp + 44) % 937 == 0
+        && (timestamp + 50) % 37 == 0
+        && (timestamp + 61) % 17 == 0
+}
+
+#[derive(Debug)]
+struct Rule {
+    offset: u64,
+    bus_id: u64,
+}
+
+fn find_timestamp(rules: &str) -> u64 {
+    // Stolen from reddit
+    rules
+        .split(",")
+        .enumerate()
+        .flat_map(|(index, bus)| {
+            bus.parse::<u64>().ok().map(|bus_id| Rule {
+                offset: index as u64,
+                bus_id,
+            })
+        })
+        .sorted_by_key(|rule| rule.bus_id)
+        .fold(
+            (0, 1),
+            |(possible_solution, least_common_denominator), rule| {
+                let mut new_possible_solution = possible_solution;
+                while (new_possible_solution + rule.offset) % rule.bus_id != 0 {
+                    new_possible_solution += least_common_denominator;
+                }
+                (
+                    new_possible_solution,
+                    least_common_denominator * rule.bus_id,
+                )
+            },
+        )
+        .0
+}
+
 pub fn run() {
     let airport = Airport::maybe_from(include_str!("data/13/1")).expect("Failed to parse");
     let (delay, bus) = airport.find_next_departure().expect("No departure found");
-    println!("{}", delay * bus.id)
+    println!("{}", delay * bus.id);
+    println!(
+        "{}",
+        find_timestamp(
+            include_str!("data/13/1")
+                .lines()
+                .skip(1)
+                .next()
+                .expect("invalid input")
+        )
+    );
 }
